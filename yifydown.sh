6f9bdeda4d35fd0191eeca545babc82187de7de0
Benjamín <Lutarisco@users.noreply.github.com>
2016-12-10 20:39:21 -0300

    Added spanish subtitles and improved syntax
    Now downloads spanish subtitles from yifysubtitles.com (it was too much for me to add it as an option by now...).
    Variable names are not capitals anymore, and all variables are wrapped inside those beautiful double quotes and braces.

--- yifydown.sh
+++ yifydown.sh
@@ -1,134 +1,149 @@
 #!/bin/bash
 #
-# YIFY movies downloader, using YTS.ag Web API, by Lutarisco (GNU GPLv3)
+# YIFY movies downloader, using yts.ag web api, by Lutarisco (GNU GPLv3)
 
-# Define usage.
+# define usage
 function usage () {
-  cat <<EOF
-  Usage: $(basename $0) [options] [query]
-  Options:
+  cat <<eof
+  usage: $(basename "${0}") [options] [query]
+  options:
   -h | help
   -q | quality [720p|1080p|3D]
   -f | just download first result"
-EOF
+eof
   exit 0
 }
 
-# Parse options
+# parse options
 while getopts ":fq:h" opt; do
-  case $opt in
-    f ) RESULT=0 ;;
-    q ) QUALITY="$OPTARG" ;;
+  case "${opt}" in
+    f ) result='0' ;;
+    q ) quality="${OPTARG}" ;;
     h ) usage ;;
     \?) usage ;;
   esac
 done
-shift $(($OPTIND - 1))
+shift "$((${OPTIND} - 1))"
 
-if [ -z "$1" ]; then
-  echo "ERROR: no query given"
+if [ -z "${1}" ]; then
+  echo "error: no query given"
   usage
   exit 1
 fi
 
-QUERY="$@"
-URL="https://yts.ag/api/v2/list_movies.json?query_term=${QUERY// /+}&sort_by=year&order_by=asc&limit=50"
+query="${@}"
+url="https://yts.ag/api/v2/list_movies.json?query_term=${query// /+}&sort_by=year&order_by=asc&limit=50"
+api='https://yts.ag/api/v2/list_movies.json'
 
-# If GET doesn't return successfully (200), exit.
-get=$(curl -sw %{http_code} -o /dev/null https://yts.ag/api/v2/list_movies.json)
-if [ "$get" != 200 ]; then
-  echo "HTTP Error: $get"
+# if get doesn't return successfully (200), exit.
+get="$(curl -sw %{http_code} -o /dev/null "${api}")"
+if [ "${get}" != 200 ]; then
+  echo "error: http status ${get}"
   exit 1
 fi
 
 
-DATA="$(curl -s "$URL")"
-RESULTS="$(echo "$DATA" | jshon -e data -e movie_count -u)"
+data="$(curl -s "${url}")"
+results="$(echo "${data}" | jshon -e data -e movie_count -u)"
 
-if [ -n "$RESULT" ]; then
-  echo "$(echo "$DATA" | jshon -e data -e movies -e 0 -e title_long -u)"
-elif [ "$RESULTS" -eq 0 ]; then
-  echo 'Sorry, no results found.'
+if [ -n "${result}" ]; then
+  echo "$(echo "${data}" | jshon -e data -e movies -e 0 -e title_long -u)"
+elif [ "${results}" -eq 0 ]; then
+  echo 'sorry, no results found.'
   exit 1
-elif [ "$RESULTS" -eq 1 ]; then
+elif [ "${results}" -eq 1 ]; then
   echo '1 movie found.'
 else
-  echo "$RESULTS movies found."
+  echo "${results} movies found."
 fi
 
-# Prompt movie confirmation.
-until [ -n "$RESULT" ]; do
-  for v in $(seq 0 "$((${RESULTS} - 1))"); do
+# prompt movie confirmation.
+until [ -n "${result}" ]; do
+  for v in $(seq 0 "$((${results} - 1))"); do
     while [ $? = 0 ]; do
-      echo "Is this the correct movie? (y/yes)(n/no)(s/summary)(q/quit)"
-      echo "$(echo "$DATA" | jshon -e data -e movies -e "$v" -e title_long -u)"
-      read -sn 1 BOOL
-      if [ "$BOOL" = y ]; then
-        RESULT="$v"
+      echo "is this the correct movie? (y/yes)(n/no)(s/summary)(q/quit)"
+      movies="$(echo "${data}" | jshon -e data -e movies)"
+      echo "$(echo "${movies}" | jshon -e "${v}" -e title_long -u)"
+      read -sn 1 bool
+      if [ "${bool}" = y ]; then
+        result="${v}"
         break 3
-      elif [ "$BOOL" = n ]; then
-        echo "Sorry, please try again."
+      elif [ "${bool}" = n ]; then
+        echo "sorry, please try again."
         break 1
-      elif [ "$BOOL" = s ]; then
-        echo "$DATA" | jshon -e data -e movies -e $v -e summary -u
+      elif [ "${bool}" = s ]; then
+        echo "${data}" | jshon -e data -e movies -e ${v} -e summary -u
         echo
-      elif [ "$BOOL" = q ]; then
+      elif [ "${bool}" = q ]; then
         exit 0
       else
-        echo "Sorry, please try again."
+        echo "sorry, please try again."
         break 1
       fi
     done
   done
 done
 
-SLUG="$(echo "$DATA" | jshon -e data -e movies -e "$RESULT" -e slug -u)"
-TORRENTS="$(echo "$DATA" | jshon -e data -e movies -e "$RESULT" -e torrents)"
-TORRENTNUMBERS="$(echo "$TORRENTS" | jshon -l)"
+slug="$(echo "${data}" | jshon -e data -e movies -e "${result}" -e slug -u)"
+torrents="$(echo "${data}" | jshon -e data -e movies -e "${result}" -e torrents)"
+torrentnumbers="$(echo "${torrents}" | jshon -l)"
+imdb="$(echo "${data}" | jshon -e data -e movies -e "${result}" -e imdb_code -u)"
 
-# Prompt quality confirmation.
-if [ -n "$QUALITY" ]; then
-  for v in $(seq 1 "$TORRENTNUMBERS"); do
-    if [ "$QUALITY" = "$(echo "$TORRENTS" | jshon -e "$v" -e quality -u)" ]; then
-      TORRENTNUMBER="$v"
+# prompt quality confirmation.
+if [ -n "${quality}" ]; then
+  for v in $(seq 1 "${torrentnumbers}"); do
+    if [ "${quality}" = "$(echo "${torrents}" | jshon -e "${v}" -e quality -u)" ]; then
+      torrentnumber="${v}"
     fi
   done
-  if [ -z "$TORRENTNUMBER" ]; then
-  echo "Sorry, there is not such quality available."
-  QUALITY=""
+  if [ -z "${torrentnumber}" ]; then
+  echo "sorry, there is not such quality available."
+  quality=""
   fi
 fi
-until [ -n "$QUALITY" ]; do
-  echo "Available qualities:"
-  echo "("$(for v in $(seq 1 $TORRENTNUMBERS); do echo "$TORRENTS" \
-    | jshon -e "$v" -e quality -u; done \
-    | while read v; do echo -n "$v/"; done \
-    | sed "s+/+)+$TORRENTNUMBERS")"(quit)"
-  read QUALITY
+until [ -n "${quality}" ]; do
+  echo "available qualities:"
+  echo "("$(for v in $(seq 1 ${torrentnumbers}); do echo "${torrents}" \
+    | jshon -e "${v}" -e quality -u; done \
+    | while read v; do echo -n "${v}/"; done \
+    | sed "s+/+)+${torrentnumbers}")"(quit)"
+  read quality
 
-  for v in $(seq 1 "$TORRENTNUMBERS"); do
-    if [ "$QUALITY" = "$(echo "$TORRENTS" | jshon -e "$v" -e quality -u)" ]; then
-      TORRENTNUMBER="$v"
+  for v in $(seq 1 "${torrentnumbers}"); do
+    torrqual="$(echo "${torrents}" | jshon -e "${v}" -e quality -u)"
+    if [ "${quality}" = "${torrqual}" ]; then
+      torrentnumber="${v}"
     fi
   done
 
-  if [ "$QUALITY" = "quit" ]; then
+  if [ "${quality}" = "quit" ]; then
     exit 0
-  elif [ -z "$TORRENTNUMBER" ]; then
-    QUALITY=""
-    echo "Sorry, please try again."
-  elif [ -z "$QUALITY" ]; then
-    echo "Sorry, please try again."
+  elif [ -z "${torrentnumber}" ]; then
+    quality=""
+    echo "sorry, please try again."
+  elif [ -z "${quality}" ]; then
+    echo "sorry, please try again."
   fi
 done
 
-# Finally, construct Magnet URL and download.
-HASH=$(echo $TORRENTS | jshon -e $TORRENTNUMBER -e hash -u)
-SIZE=$(echo $TORRENTS | jshon -e $TORRENTNUMBER -e size -u)
-MAGNET="magnet:?xt=urn:btih:$HASH&dn=${SLUG//-/+}&tr=udp://glotorrents.pw:6969/\
-announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresill\
-e.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppe\
-rsurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenab\
-g.ch:1337&tr=udp://tracker.internetwarriors.net:1337"
-echo "File size: $SIZE"$'\n'"Now downloading..."
-aria2c --seed-time=0 "$MAGNET"
+# download spanish subs if any.
+subsdata="$(curl -s "http://api.yifysubtitles.com/subs/${imdb}")"
+if [ "$(echo "${subsdata}" | jshon -e success -u)" == true ]; then
+  subs="$(echo "${subsdata}"| jshon -e subs -e "${imdb}")"
+  spanishsub="$(echo "${subs}" | jshon -e spanish -e 1 -e url -u)"
+  echo -n "downloading spanish subtitles..."
+  curl -LOs "http://www.yifysubtitles.com${spanishsub}"
+  echo ' done.'
+fi
+
+# finally, construct magnet url and download.
+hash="$(echo "${torrents}" | jshon -e "${torrentnumber}" -e hash -u)"
+size="$(echo "${torrents}" | jshon -e "${torrentnumber}" -e size -u)"
+magnet="magnet:?xt=urn:btih:${hash}&dn=${slug//-/+}&tr=udp://tracker.opentrackr.org:13\
+37/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorren\
+t.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.or\
+g:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337"
+
+echo "file size: ${size}"
+echo "now downloading..."
+aria2c --file-allocation=none --seed-time=0 "${magnet}"
